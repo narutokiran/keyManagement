f03b316d220311d43951c67a41bbe610bc423303
kiran s <Kiran@kirans-MacBook-Pro.local>
2018-02-09 22:02:42 -0500

    Script accepts password

--- create_user.sh
+++ create_user.sh
@@ -7,6 +7,6 @@ newid=$((maxid+1))
 sudo dscl . -create /Users/$1 UniqueID ${newid}
 sudo dscl . -create /Users/$1 PrimaryGroupID 80
 sudo dscl . -create /Users/$1 NFSHomeDirectory /Users/$1
-sudo dscl . -passwd /Users/$1 qwerty
+sudo dscl . -passwd /Users/$1 $2
 cp -R /System/Library/User\ Template/English.lproj /Users/$1
 sudo chown -R $1:staff /Users/$1
