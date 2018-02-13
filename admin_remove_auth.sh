70cf44c114f0e9b392deb39f398ac3ccebc8e10d
kiran s <Kiran@kirans-MacBook-Pro.local>
2018-02-13 09:40:30 -0500

    Fixed permission issue

--- admin_remove_auth.sh
+++ admin_remove_auth.sh
@@ -3,3 +3,4 @@
 admin=$1
 grep -v $2 /Users/${admin}/.ssh/authorized_keys > /Users/${admin}/.ssh/tmp_auth
 mv /Users/${admin}/.ssh/tmp_auth /Users/${admin}/.ssh/authorized_keys
+sudo chown ${admin}:staff /Users/${admin}/.ssh/authorized_keys
